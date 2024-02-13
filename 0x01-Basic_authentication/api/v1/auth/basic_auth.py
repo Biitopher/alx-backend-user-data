89f18e5bbc57a5914ac85ab3c482df8d63e45ee6
Biitopher <biitopher@gmail.com>
2024-02-13 20:18:38 +0000

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -20,3 +20,21 @@ class BasicAuth(Auth):
         base64_credentials = authorization_header.split(" ", 1)[1]
 
         return base64_credentials
+
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """Check if base64_authorization_header is None"""
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_value = decoded_bytes.decode('utf-8')
+            return decoded_value
+        except base64.binascii.Error:
+            return None
