167b1bb55166009f20cac3f2e08f367560f4cc94
Biitopher <biitopher@gmail.com>
2024-02-13 11:16:23 +0000

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,16 +2,28 @@
 """API authentication class"""
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
     """class to manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Validates if a request needs authentication"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Retrieves the Authorization header from the request"""
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
