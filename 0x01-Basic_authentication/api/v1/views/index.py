f09ea9185c5474de788757ab26e2582d2bf67cf0
Biitopher <biitopher@gmail.com>
2024-02-13 00:18:06 +0000

    Task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint() -> str:
+    """Triggering 401 Unauthorized error using abort"""
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """Triggering 403 forbidden error by using abort"""
+    abort(403)
