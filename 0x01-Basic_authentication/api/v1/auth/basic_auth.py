48c727b1f8248cde3b32a0bcbcc1f9b220dde120
Biitopher <biitopher@gmail.com>
2024-02-14 06:48:03 +0000

    Pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Class BasicAuth  that inherits from Auth""" 
+"""Class BasicAuth  that inherits from Auth"""
 import base64
 from api.v1.auth.auth import Auth
 from typing import TypeVar
@@ -8,7 +8,7 @@ from models.user import User
 
 class BasicAuth(Auth):
     """Creates class BasicAuth"""
-    def extract_base64_authorization_header(self, 
+    def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """Decodes base64 string"""
         if authorization_header is None:
@@ -22,7 +22,6 @@ class BasicAuth(Auth):
         base64_credentials = authorization_header.split(" ", 1)[1]
 
         return base64_credentials
-
 
     def decode_base64_authorization_header(self,
                                            base64_authorization_header:
@@ -41,7 +40,6 @@ class BasicAuth(Auth):
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
 
-
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header:
                                  str) -> (str, str):
@@ -56,9 +54,8 @@ class BasicAuth(Auth):
             return None, None
 
         user_email, user_password = (decoded_base64_authorization_header.split
-                                    (':', 1))
+                                     (':', 1))
         return user_email, user_password
-
 
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
@@ -80,7 +77,6 @@ class BasicAuth(Auth):
 
         return None
 
-
     def current_user(self, request=None) -> TypeVar('User'):
         """Retrieve the authorization header from the request"""
         authorization_header = self.authorization_header(request)
@@ -89,7 +85,8 @@ class BasicAuth(Auth):
             self.extract_base64_authorization_header(authorization_header)
         )
 
-        user_email, user_password = self.extract_user_credentials(decoded_value)
+        user_email, user_password = (self.extract_user_credentials
+                                     (decoded_value))
 
         user_instance = self.user_object_from_credentials(user_email,
                                                           user_password)
