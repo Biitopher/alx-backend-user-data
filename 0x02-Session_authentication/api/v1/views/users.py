b3037089527d1eac3e553a03cb620c4873757ceb
Biitopher <biitopher@gmail.com>
2024-02-14 22:50:28 +0300

    Task 0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,8 +30,8 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == 'me' and request.current_user is None:
         abort(404)
     elif user_id == 'me' and request.current_user is not None:
-        # Return authenticated User in a JSON response
-        return jsonify(request.current_user.to_dict()), 200
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
