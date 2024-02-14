5d2fdc89e393e04d9a21de67587e8ed5ecd333ac
Biitopher <biitopher@gmail.com>
2024-02-14 08:55:15 +0000

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -60,21 +60,15 @@ class BasicAuth(Auth):
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
         """Check if user_email is None or not a string"""
-        if user_email is None or not isinstance(user_email, str):
-            return None
-
-        if user_pwd is None or not isinstance(user_pwd, str):
-            return None
-
-        users = User.search({'email': user_email})
-
-        if not users:
-            return None
-
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
-
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
