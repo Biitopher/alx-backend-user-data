2803e9229e7db58b742c2a6b2a073d54f0f941a6
Biitopher <biitopher@gmail.com>
2024-02-15 15:06:55 +0300

    Task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -13,17 +13,13 @@ def session_login():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not email:
+    if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
-    if not password:
+    if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
-
-    user = User.search({'email': email})
-    if not user:
+    users = User.search({"email": email})
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
-    if not user[0].is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-
     for user in users:
         if user.is_valid_password(password):
             session_id = auth.create_session(user.id)
