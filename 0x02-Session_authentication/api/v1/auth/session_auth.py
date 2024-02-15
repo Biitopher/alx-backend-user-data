1609df9742f7c2a3165a529b4d36d47870caea3f
Biitopher <biitopher@gmail.com>
2024-02-15 14:48:53 +0300

    Task 7

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -39,32 +39,3 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
 
         return User.get(user_id)
-
-    @app.route('/auth_session/login', methods=['POST'])
-    def session_login():
-        """Session authentication"""
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        if not email:
-            return jsonify({"error": "email missing"}), 400
-
-        if not password:
-            return jsonify({"error": "password missing"}), 400
-
-        user = User.search({'email': email})
-
-        if not user:
-            return jsonify({"error": "no user found for this email"}), 404
-
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
-
-        session_id = auth.create_session(user.id)
-        response_data = user.to_json()
-
-        session_cookie_name = app.config.get('SESSION_NAME', 'session_id')
-        response = jsonify(response_data)
-        response.set_cookie(session_cookie_name, session_id)
-
-        return response
