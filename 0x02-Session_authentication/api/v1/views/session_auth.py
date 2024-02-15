b91052f7b75438d553f10589f2a0cc703f8256c6
Biitopher <biitopher@gmail.com>
2024-02-15 13:58:39 +0000

    Import

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Module of Users views"""
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
 from api.v1.auth.session_auth import SessionAuth
+from os import getenv
 
 
 auth = SessionAuth()
