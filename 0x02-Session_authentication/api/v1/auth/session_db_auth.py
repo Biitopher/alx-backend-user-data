0459c28dc525060f4171cb400bfe2891ed6b055a
Biitopher <biitopher@gmail.com>
2024-02-15 23:30:35 +0000

    Task 10

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Sessions in database"""
+from models.user_session import UserSession
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    def create_session(self, user_id=None):
+        """Create sessional function"""
+        session_id = super().create_session(user_id)
+        if session_id:
+            user_session = UserSession(user_id=user_id, session_id=session_id)
+            user_session.save()
+            return session_id
+        return None
+
+    def user_id_for_session_id(self, session_id=None):
+        """Get user session id"""
+        if session_id is None:
+            return None
+
+        user_session = UserSession.load_from_file(session_id)
+        if not user_session:
+            return None
+
+        return user_session.user_id
+
+    def destroy_session(self, request=None):
+        """Destroy the session""
+        if request is None:
+            return
+
+        session_id = request.cookies.get("session_id")
+        if session_id:
+            user_session = UserSession.load_from_file(session_id)
+            if user_session:
+                user_session.delete()
---
+++ 0x02-Session_authentication/models/user_session.pyuser_session.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Handle User session storage"""
+from models.base import Base
+
+class UserSession(Base):
+    """The class user session"""
+    def __init__(self, *args, user_id=None, session_id=None, **kwargs):
+        """Initialize the session"""
+        super().__init__(*args, **kwargs)
+        self.user_id = user_id
+        self.session_id = session_id
