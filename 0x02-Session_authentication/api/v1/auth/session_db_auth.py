ea422541120a4c9b1710b65695aeb0da2a7bbab2
Biitopher <biitopher@gmail.com>
2024-02-16 03:29:09 +0300

    Task 10

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Sessions in database"""
 from models.user_session import UserSession
-from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 class SessionDBAuth(SessionExpAuth):
@@ -30,7 +30,7 @@ class SessionDBAuth(SessionExpAuth):
         if request is None:
             return
 
-        session_id = request.cookies.get("session_id")
+        session_id = SessionDBAuth().session_cookie(request)
         if session_id:
             user_session = UserSession.load_from_file(session_id)
             if user_session:
