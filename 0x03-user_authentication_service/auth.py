078910990cb3ea7fea0d438f18fb65bf5f4e3a0d
Biitopher <biitopher@gmail.com>
2024-02-20 13:16:29 +0000

    Task 3

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 """Define hash password method"""
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password with salt using bcrypt"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
