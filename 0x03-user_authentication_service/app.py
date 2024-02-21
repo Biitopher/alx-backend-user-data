4a616de7a2e248eb3da51d796859f3b66a0c1715
Biitopher <biitopher@gmail.com>
2024-02-21 13:03:16 +0000

    Task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Setting up basic flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort,  make_response, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -29,6 +29,22 @@ def register_user():
     except ValueError as e:
         response_data = {"message": str(e)}
         return jsonify(response_data), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Get email and password from the request form data"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+        response = jsonify({'email': email, 'message': 'logged in'})
+        response.set_cookie("session_id", session_id)
+        return response
 
 
 if __name__ == "__main__":
