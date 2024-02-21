10906257b4f0130512f7aab89cdc9b05d9f127ec
Biitopher <biitopher@gmail.com>
2024-02-21 11:57:45 +0000

    Task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,7 +50,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             if user:
-                session_id = _generate_uuid()
+                session_id = self._generate_uuid()
                 self._db.update_user_session_id(user.id, session_id=session_id)
                 return session_id
 
